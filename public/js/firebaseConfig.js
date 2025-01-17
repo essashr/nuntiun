a1bdd10c7783c4bbf3f9495df280887f08c1ea91
Thomaz Silva <thomazaugustoth13@gmail.com>
2025-01-17 00:01:09 -0300

    adicionando o google analytics a produção

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js';
 
 async function fetchFirebaseConfig() {
     const response = await fetch('/api/firebaseAPI');
@@ -9,6 +10,9 @@ async function fetchFirebaseConfig() {
 async function initializeFirebase() {
     const firebaseConfig = await fetchFirebaseConfig();
     const app = initializeApp(firebaseConfig);
+    if (firebaseConfig.measurementId) {
+        getAnalytics(app);
+    }
     return app;
 }
 
